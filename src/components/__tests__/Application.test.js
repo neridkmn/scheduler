93ebd2338ca2d936289b06df2ff89502ec231bff
Neriman Dikmen <sanerneriman@gmail.com>
2023-10-28 18:16:53 -0400

    Add first Application.js test

---
+++ src/components/__tests__/Application.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, waitForElement, fireEvent } from "@testing-library/react";
+import Application from "components/Application";
+
+
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
+
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+});
